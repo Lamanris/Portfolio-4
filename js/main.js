b82ab1dcf88c2ae56ac67f7e4ab91a4bdf541823
Lamanris <Dreamer19042002@gmail.com>
2020-11-29 19:31:53 +0600

    test11

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.header__burger').click(function (event) {
         $('.header__burger,.header__rightSide,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
+        $('html').toggleClass('lock');
     });
 });
 
